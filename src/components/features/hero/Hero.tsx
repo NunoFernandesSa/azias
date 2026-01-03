2492791341a9d17f3dd947cc07f08e6197ae8a15
NunoF <n.fernandes.contact@gmail.com>
2026-01-03 20:17:18 +0100

    feat: enhance Hero component with new button styles and add scroll indicator

--- src/components/features/hero/Hero.tsx
+++ src/components/features/hero/Hero.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { BiCalendar, BiPhone } from "react-icons/bi";
 import { Button } from "../../ui/button";
+import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 
 export default function Hero() {
   return (
@@ -12,10 +13,11 @@ export default function Hero() {
         alt="Logo da equipa FC Azias"
         fill
         className="absolute object-cover object-center brightness-75 -z-10"
+        priority
       />
 
       {/* Content */}
-      <div className="w-full h-screen flex flex-col justify-end items-center bg-linear-to-t from-primary via-primary/70 to-background/20 gap-6 pb-20">
+      <div className="w-full h-screen flex flex-col justify-end items-center bg-linear-to-t from-primary via-primary/70 to-background/20 gap-6 pb-6">
         <h2 className="text-6xl md:text-8xl lg:text-10xl xl:text-[12rem] font-bold text-primary-foreground/70">
           AZIAS FC
         </h2>
@@ -24,17 +26,36 @@ export default function Hero() {
         </p>
 
         <div className="flex flex-col sm:flex-row items-center justify-center gap-6 w-full px-6">
-          <Button asChild variant={"default"} size="lg">
+          <Button
+            asChild
+            variant={"default"}
+            size="lg"
+            className="w-full sm:w-1/3 lg:w-1/4"
+          >
             <Link href="/calendario">
               <BiCalendar /> Ver calendário
             </Link>
           </Button>
 
-          <Button asChild variant={"outline"} size="lg">
+          <Button
+            asChild
+            variant={"outline"}
+            size="lg"
+            className="w-full sm:w-1/3 lg:w-1/4"
+          >
             <Link href="/contacto">
               <BiPhone /> Entrar em contato
             </Link>
           </Button>
+        </div>
+
+        <div className="mt-8 animate-bounce">
+          <Link href="#nextMatch">
+            <MdKeyboardDoubleArrowDown
+              size={40}
+              className="text-secondary/50"
+            />
+          </Link>
         </div>
       </div>
     </div>
