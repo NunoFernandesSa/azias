cf6fae21f2bd8af500a4af252d2d6131e9d97b22
NunoF <n.fernandes.contact@gmail.com>
2026-01-04 02:59:07 +0100

    Updates site metadata and refines footer layout
    Improves SEO and social sharing by expanding site metadata with keywords, Open Graph, and icons. Refactors footer for better structure, responsiveness, and consistency across devices.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@styles/globals.css";
-import { Navbar } from "../components/common/Navbar";
-import Container from "../components/common/Container";
-import { useIsMobile } from "../hooks/useIsMobile";
 import { ClientLayout } from "../components/layout/ClientLayout";
 import Footer from "../components/common/Footer";
+import "@styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,8 +15,32 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Azias FC",
-  description: "",
+  title: "Azias FC - Equipa de Futebol Amador | Liga Inatel Portugal",
+  description:
+    "O Azias FC é uma equipa de futebol amador de Ponte da Barca que participa na Liga Inatel em Portugal. Combinando competição e um ambiente amigável. Futebol de competição com espírito desportivo e convívio. Junte-se à nossa equipa!",
+  keywords: [
+    "Azias FC",
+    "Futebol amador em Portugal",
+    "Ligua Inatel",
+    "equipa de futebol amador Portugal",
+    "clube amador Portugal",
+    "futebol amador Portugal",
+    "Liga Inatel",
+    "Inatel football",
+  ].join(", "),
+  icons: [
+    { url: "/favicon.ico" },
+    { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+  ],
+  openGraph: {
+    title: "Azias FC - Equipa de Futebol Amador | Liga Inatel Portugal",
+    description:
+      "Junta-te ao Azias FC, uma equipa de futebol amador que participa na Liga Inatel em Portugal. Ambiente amigável e competição desportiva!",
+    type: "website",
+    locale: "pt_PT",
+    siteName: "Azias FC",
+  },
 };
 
 export default function RootLayout({
--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,35 +2,36 @@ import { Button } from "../ui/button";
 
 export default function Footer() {
   return (
-    <footer className="lg:ml-48 flex-1 bg-primary text-primary-foreground">
-      <div className="flex flex-col items-center gap-3 py-6">
-        <p className="text-xl font-semibold">Clube Amador INATEL</p>
-        <div className="flex gap-0 md:gap-8 w-full justify-center items-center">
-          <img
-            src="/images/azias-fc-logo.webp"
-            alt="Logo Inatel"
-            className="w-30 md:w-36"
-          />
-          <img
-            src="/images/logo-inatel.webp"
-            alt="Logo Inatel"
-            className="w-30 md:w-36"
-          />
+    <footer className="w-full bg-primary text-primary-foreground">
+      <div className="lg:ml-48 md:px-8">
+        <div className="flex flex-col items-center gap-3 py-6">
+          <p className="text-xl font-semibold">Clube Amador INATEL</p>
+          <div className="flex gap-0 md:gap-8 w-full justify-center items-center">
+            <img
+              src="/images/azias-fc-logo.webp"
+              alt="Logo Inatel"
+              className="w-30 md:w-36"
+            />
+            <img
+              src="/images/logo-inatel.webp"
+              alt="Logo Inatel"
+              className="w-30 md:w-36"
+            />
+          </div>
         </div>
-      </div>
+        <div className="container mx-auto h-16 flex flex-col md:flex-row items-center justify-between">
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} Azias FC - Todos os direitos
+            reservados
+          </p>
 
-      <div className="container mx-auto h-16 flex flex-col md:flex-row items-center justify-between">
-        <p className="text-sm">
-          &copy; {new Date().getFullYear()} Azias FC - Todos os direitos
-          reservados
-        </p>
-
-        <Button
-          variant="link"
-          className="text-sm font-light cursor-pointer text-primary-foreground ease-in-out transition-all duration-500 hover:text-primary-foreground/70"
-        >
-          Termos e Condições
-        </Button>
+          <Button
+            variant="link"
+            className="text-sm font-light cursor-pointer text-primary-foreground ease-in-out transition-all duration-500 hover:text-primary-foreground/70"
+          >
+            Termos e Condições
+          </Button>
+        </div>
       </div>
     </footer>
   );
