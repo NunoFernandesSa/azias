a2bbf6047429989c931791b59dfed1c7f2781004
NunoF <n.fernandes.contact@gmail.com>
2026-01-05 19:38:38 +0100

    fix: adjust column count in PlayerSection for improved layout consistency

--- src/components/features/team/PlayerSection.tsx
+++ src/components/features/team/PlayerSection.tsx
@@ -5,7 +5,7 @@ export function PlayersSection({ sectionName, children }: PlayerSectionProps) {
     <>
       <p className="text-primary font-bold text-xl">{sectionName}</p>
       <hr />
-      <div className="px-2 xl:px-8 py-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-6 gap-6">
+      <div className="px-2 xl:px-8 py-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6">
         {children}
       </div>
     </>
