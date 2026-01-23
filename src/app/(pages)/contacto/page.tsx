382933c2cc0c5dd9a3a6d1ca031cebd4f8dd39c0
NunoF <n.fernandes.contact@gmail.com>
2026-01-23 21:44:37 +0100

    feat: implement ContactPage layout with Container, ContactCard, and MapsCard components

--- src/app/(pages)/contacto/page.tsx
+++ src/app/(pages)/contacto/page.tsx
@@ -1,3 +1,13 @@
+// ----------- components -----------
+import Container from "@/src/components/common/Container";
+import ContactCard from "@/src/components/features/contact/ContactCard";
+import MapsCard from "@/src/components/features/contact/MapsCard";
+
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <Container className="mx-3 lg:mx-16 my-30 lg:my-16 pt-6 lg:pt-0 flex flex-col gap-12">
+      <ContactCard />
+      <MapsCard />
+    </Container>
+  );
 }
